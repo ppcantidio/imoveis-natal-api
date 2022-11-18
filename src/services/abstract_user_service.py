50d7c161d17db1437ce0b7d9d459ea0eb663692a
Pedro Cantidio <ppcantidio@gmail.com>
2022-11-18 01:01:41 +0000

    db

--- src/services/abstract_user_service.py
+++ src/services/abstract_user_service.py
@@ -1,26 +1,28 @@
 from src.database import UserDB
 from src.utils.errors import AbstractErrror
 
-class AbstractUserService:
-    def __init__(self, user_object):
-        self.user_object = user_object
-        self.db = UserDB()
 
-    def create_user(self):
-        if self.__user_existence():
+class AbstractUserService:
+    def __init__(self, user_db):
+        self.db = user_db
+
+    def create_user(self, user_object):
+        if self.__user_existence(user_object):
             raise AbstractErrror('user alredy exist')
 
-        
+        user_object.password = self.__generate_password_hash(user_object)
 
-        
+        self.db.register_user(user_object)
 
-        
+    def get_user(self, user_id):
+        user = self.db.get_user_by_id(user_id)
+        return user
 
-    def get_user(self):
-        pass
 
-
-    def __user_existence(self):
-        if self.db.get_object_by_email(self.user.email):
+    def __user_existence(self, user_object):
+        if self.db.get_object_by_email(user_object.email):
             return True
         return False
+
+    def __generate_password_hash(self, user_object):
+        return user_object.password
